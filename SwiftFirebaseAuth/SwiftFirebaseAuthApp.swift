f58ba6dd738e4e21b8a14c9534819d2db20ef364
Shrikanta Mazumder <shrikantamazumder@gmail.com>
2021-09-12 23:17:17 +0600

    Initial Commit

---
+++ SwiftFirebaseAuth/SwiftFirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFirebaseAuthApp.swift
+//  SwiftFirebaseAuth
+//
+//  Created by Shrikanta Mazumder on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
